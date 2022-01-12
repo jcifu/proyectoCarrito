799f6b893c3e988df697057a95622fcf508890b1
Javier Cifuentes <jcifuentescalderon@gmail.com>
2022-01-12 19:02:11 -0300

    proyecto final

---
+++ config-server/src/test/java/com/jcifu/store/configserver/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcifu.store.configserver.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ eureka-ms/src/test/java/com/example/store/eurekams/EurekaMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.store.eurekams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
